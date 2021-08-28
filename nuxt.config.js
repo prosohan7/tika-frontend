8047657bd414e85c3f6e80a1834ef87c2eeadaad
prosohan7 <89298094+prosohan7@users.noreply.github.com>
2021-08-29 02:25:56 +0600

    nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,12 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
+    '@nuxtjs/axios',
   ],
+
+  axios: {
+    baseURL: 'http://tika.test/api'
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <Tutorial/>
+ <div>
+ 	<h1>Check:{{apiValue}}</h1>
+ 	<Tutorial/>
+ </div>
 </template>
 
 <script>
-export default {}
+export default {
+	data() {
+		return {
+			apiValue: ''
+		}
+	},
+	mounted() {
+		this.testBackendRequest();
+	},
+	methods: {
+		testBackendRequest() {
+			this.$axios.get('/test').then(res => {
+				console.log(res.data);
+			});
+		}
+	}
+}
 </script>
